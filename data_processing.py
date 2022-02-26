5436ff2088c8f16991e50034fafb499c8f0f9a44
pattanaikay <pratikpattanaik821@gmail.com>
2022-02-26 11:59:02 +0530

    Revert "added a new function sum"
    This reverts commit f132a1a5150e15ed1301d822af314617e9c2daa4.

--- data_processing.py
+++ data_processing.py
@@ -1,11 +1,1 @@
 print("Adding all the data processing code related to fraud detection")
-
-a= 5
-b= 7
-
-def sum(a, b):
-    c = a + b
-    return a
-
-#adding wrong version
-print("Added a new function sum")
